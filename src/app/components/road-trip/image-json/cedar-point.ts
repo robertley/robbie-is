bd9d5e56d0ce43d6e06217020abb781748ee8a58
robertley <robert.ley94@gmail.com>
2021-09-07 17:32:20 -0500

    migrated from static assets to google photos

--- src/app/components/road-trip/image-json/cedar-point.ts
+++ src/app/components/road-trip/image-json/cedar-point.ts
@@ -1,34 +1,34 @@
 export default [
     {
-        "src": "assets/road-trip/cedar-point/IMG_2192.jpg",
+        "src": "https://lh3.googleusercontent.com/hs1BkGzo_IIBJSL_MZTCq2gtxQTUFvKO7t80kja_8w1ORlsuQPPhB8W1GUYNNJEJGAPQleEqZC24pxhIPHZ3jWwFsVrL8QLya8_DAzEvj98favZeip4MgILefIn7FdcgW21Ux_Of=w2400",
         "caption": "going to the park"
     },
     {
-        "src": "assets/road-trip/cedar-point/IMG_2193.jpg",
+        "src": "https://lh3.googleusercontent.com/ji_izL5gFzcC_KcMf4Ui_zp44KybYqTNyFSJKmenm_VtGaclEp67SeZ0cQo8CiibKmqHLKAz2PP64kOEf3M_TO3mYIQX7RbM4S-Mj1vvA7ctuQpkSOhL1niVBnXdCcFohr8EPC0y=w2400",
         "caption": null
     },
     {
-        "src": "assets/road-trip/cedar-point/IMG_2196.gif",
+        "src": "https://lh3.googleusercontent.com/a3IZyZYe5W4q8f6JKudb8QX4NC4rSSJx0HpTUbSbr3LPzvcF9kNYBsWslZt_vvIBBu6Kmh9CJzgXe_yXZP_EqL65ctfxyOizbZ5g8sVupHKyCS86Gg3s6O6xMRTulR_VzBtA1Yos=w2400",
         "caption": null
     },
     {
-        "src": "assets/road-trip/cedar-point/IMG_2198.jpg",
+        "src": "https://lh3.googleusercontent.com/bS8MR4ZJ5pGNeVbNQv5Czeny1iVYzO-n9HODwYujfhax7II9lncnyxWY622geQlP7aqmcWIqlHq7lue_P82pUiLq-g4ZmEyQJRp1bNM9eZNNHeTM8BgZpmw9XRSjiEmKA2FSz1YM=w2400",
         "caption": null
     },
     {
-        "src": "assets/road-trip/cedar-point/IMG_2199.jpg",
+        "src": "https://lh3.googleusercontent.com/lfRO60etDpKECW1A7hq6fSFJozTbHnlpiORooOFOTlyZeqMzvS7xglt4DIF9xq5rs9LKaW7lzhPKdo1JnpUthHVeyPIgy0vcIbCqs7sHEiSehqgkgiq1sMt0Q_B_7eSpr4vpVkIj=w2400",
         "caption": null
     },
     {
-        "src": "assets/road-trip/cedar-point/IMG_2202.jpg",
+        "src": "https://lh3.googleusercontent.com/cwg-NUnkoY6X_uq9p6zt8lWuKPy0o19r8NDCn27-8NcXeC70fBk1z0eSdSl3i_kQWuyfLNn-OlQ2KiQfUyLrQUaUs9zT9NZJS7wuY-X_Mn-K4G_8t6KMMgPVC8l6W2_wmcGJ15lp=w2400",
         "caption": null
     },
     {
-        "src": "assets/road-trip/cedar-point/IMG_2203.jpg",
+        "src": "https://lh3.googleusercontent.com/WElUOD1jZpr5D_tRXOCa1Yq-MNfy34WgjxyslCRJCT5cnbaeQVO5htrPZ4wlSKCv3k4TxGEcKfACS5sCl2Q_iLhVDKMhCbWzo5tCKSr1r6pJ5f_H-D6mxMqPATDICG9yqExzOYWu=w2400",
         "caption": null
     },
     {
-        "src": "assets/road-trip/cedar-point/IMG_2204.jpg",
+        "src": "https://lh3.googleusercontent.com/BomtAGkeqcN8cmqwV0z4oG-wYMowq1PMKM1wl361sZEL_Wq5RYPsrbFIIlrURGiL8MO3SDOroR9B79JviTURoxSI4emcQEt-xQm3jSVqvV_4ndPTqgfTVHAufIQLk4II_KtZDyMK=w2400",
         "caption": null
     }
 ]
--- src/app/components/road-trip/image-json/chicago.ts
+++ src/app/components/road-trip/image-json/chicago.ts
@@ -1,78 +1,78 @@
 export default [
     {
-        src: "assets/road-trip/chicago/IMG_2205.jpg",
+        src: "https://lh3.googleusercontent.com/v_w-OVW2V8Y39A-22D-xBULvJ0P--tvt-m5BOJ3aRAq3rO_StfcL7i6K6-P8Y-ptWd9q56ZqCgYYRiT_OyddpvX5om9lkruV0P10rEQ5UIffq40kKL8VP0rHk2QQMLdA0SFobUo5=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2209.jpg",
+        src: "https://lh3.googleusercontent.com/psbASdsrao_ox1X5z2quSti8umLUvli8SC45FePJ0Sm_iQOD-z_tq01iSiOO_cUKoKelCSKysOlBBHuM3VIDzAco0dpJYux73B503iq40fK4HVO0fUNdaCPozxHnTltMIYqi1k2l=w2400",
         caption: "First chicago lunch: empanadas with the lake turtles."
     },
     {
-        src: "assets/road-trip/chicago/IMG_2210.jpg",
+        src: "https://lh3.googleusercontent.com/zveEMkhF8YuK8xu01xnot6WBGKU09HgOb1CPrrC8yPYBIWeGnk4dgFl75sdRIeOJgV-wk1m6bXLFJkcMs-QBorxsDHvbIRqVTzkJhVUeOTzokW5TUMaR-AK9DcDvyAkhPZs1RUQ3=w2400",
         caption: "Aformentioned turtles."
     },
     {
-        src: "assets/road-trip/chicago/IMG_2215.jpg",
+        src: "https://lh3.googleusercontent.com/NdJF_kvhlymPPQ315F2is_rdARm5k7fbHc0Q4Wbh8_vzefbjeXa6J60kl-Ky3JGoLlsXItCKhUCAZaALc8oCOuzL-LeglD8xrODXfsZ91k40PIfUXpt0G3pKPOU8eY1jfWU5_YkQ=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2221.jpg",
+        src: "https://lh3.googleusercontent.com/B9keAIxyMbaYs8E-Y1c2QhGxST5bDRZNuo9MbtwB8ey0NGXm2yq_wHw3uSJ8MrS9JpPXcX3N_udYUskIl7Ov8XYFMGLIS4sa5yKXdXJLBUlBl_vHNAkn6mGFADUkUzosArxfw89f=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2222_com.jpg",
+        src: "https://lh3.googleusercontent.com/RbC9Prto4SNk_Mrwo1EgQgB0ZaNj0QxGu-3ty3Ude1-BMgLDbIlt4KzWroHA5bZtihrTLw-psdXKDXKupcD8OZVTtsXeDuDd1NAEAJcwDHEEpwRfgZDxxwgLu2K8D23TPTGBrcIt=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2219.gif",
+        src: "https://lh3.googleusercontent.com/NLAXLij0F14SyL106VIwoonXDlxeqlcnUkxNkn6w9Ck2UoEtBCpUmOiQ671ZlTiF9wmVlcwmyzcDdjeFsaWUeVuURykqs_SQ7vsdjALnGAtTCz4GYKyambv6rlxukMoXEhr1-uy-=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2220.gif",
+        src: "https://lh3.googleusercontent.com/frJRHI2WrD1BRPLcx-BUMfS36feeTJXSk6H4NgGJkKHW_bf6VyqQbgWI3cBvOsXdxygxInWB6EhzHR5cUuB3MbyAyDF3z8UzgxN3vgMeYqbdGnODjxENZuD0HMdB-3Xntgzj2hPu=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2227.gif",
+        src: "https://lh3.googleusercontent.com/ialyauQRjDfydwKy3guNOBq6PGTs54ZovgBbD_TJtJCbrR02YrQvcYR_6oAMECr_KhPssrTK1Mm6Kj2-WUY5V9aT1sbvKYZDl1q6PVhu2u_2m728tQK-1UwrLMQhG62dm-U5UoNH=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2228.jpg",
+        src: "https://lh3.googleusercontent.com/ZMiUkcapQu5Y9KLWgBiDqBC_GUN4tK8PLK1fNudnj55rDD8Xw5JAtXBGv8jdKcPAq6DaJOsq3y64SB9LTYzArzmQMgjEu5LNjAVS7UgW4DQpFSsiFwengnvvqO2VSy3haKlQJ635=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2232.JPG",
+        src: "https://lh3.googleusercontent.com/hqRqfdA3f5W6PY11DcUZhyqibMTgAM3vE3qG1w9nzWXjO6MwHQrswk8MmEc0sM-NK_V2HMTvF8gt2DjOFAQmEtMxD0EtwyRZAY5fh2yZD-HHGGtkinLWB9P3YKueH6HqzKO9luMk=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2265.jpg",
+        src: "https://lh3.googleusercontent.com/rpRxuBEf6Y5Cgzw6wZdHR5WQtMgOF6k4vzKt7uMiKYK9F9qrGGGWIgHeDQQW5xelrVI-_ldPBXaTZWfC9-VXqqN9bP__KYUWf5Q6nbhXad0J42fzVZJWdtOb7kAEnwy4LSRhy6NC=w2400",
         caption: "First house sit with Hurley. (And Walker the cat hiding somewhere)"
     },
     {
-        src: "assets/road-trip/chicago/IMG_2267.jpg",
+        src: "https://lh3.googleusercontent.com/wyBxyLi6JgmA6DGJWNzu5wJmLVBhRPkL6nEd7asZhReC9SO4HNLqwMDIDRyGObaEXYvNo0QVyfFfGenHlOZNLVyUcEj50Vk7yOYxbA0ZcAX7Xdw-6Cgis89FxmTeqKcNVmwJOMws=w2400",
         caption: "Serious Abby vibes"
     },
     {
-        src: "assets/road-trip/chicago/IMG_2301.JPG",
+        src: "https://lh3.googleusercontent.com/osyQ9KFd0EL8B6XFmDhDvYd-OSCPA9Sh2YvEr3cYh6lh4I1de9eugG9QpDyuYlxvklV1w1L6CwzNPwB72u1fG5OPKPQtk8mCB61Htc1V5OFQIFGZXJNCyCOv2FgXw8DsUsHbZvpd=w2400",
         caption: "Found Walker"
     },
     {
-        src: "assets/road-trip/chicago/IMG_2272.jpg",
+        src: "https://lh3.googleusercontent.com/QCQTrCv2QHDeXvL39CGwLgm1adz1ObnWay8EfE-qaTZhwbsRsqe04mx73-aX7GMmsUttznW97-PFiW-h3TynnTbs2QEBgPFNrqNO6CRxim9MLDeEzd8qEQ3vecQRGYG69DSxOQkm=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2277.jpg",
+        src: "https://lh3.googleusercontent.com/XsFF7WgbEF3-f6sgRKqAoN07yUaU4D5eF1KVB9IvMbQbTw255R8Ex9atR4kCbUV1yfOJJkjePPU-ydpBw5gvHMncV5gpgkI2isfmNTGzTMcyiaXYU-Hcq5BEnZBH2BVtSxYDX5Fm=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2302.jpg",
+        src: "https://lh3.googleusercontent.com/TSFycKr0r1VwFMjzuLf27L9vjwK-SlYbce42VLEQLACzMm-yXQsV2-yS5lrHvcg0Ai5UTM_81tgcG2Vq-FIdilyPBQUYMOyXGVXHVrxqO7kesi9ViW4EzXrx-5tPJSjcPq9GF_mL=w2400",
         caption: "Met three guys at an outdoor bar (two humans, one parrot named Max)."
     },
     {
-        src: "assets/road-trip/chicago/IMG_2294.jpg",
+        src: "https://lh3.googleusercontent.com/JiW-ayzbz0CwnHwGMeHETeKmqZbDmxrTGxefi7e9UAbg7CYKV3vsbsZ7CRQHSgWtJnBkkXQceqJ3-P6E873vesJfWfUnxB2_wJDJMfb_eu1ux2RtS-QY3b1UGtuEKzdNmPsJ9oN0=w2400",
         caption: null
     },
     {
-        src: "assets/road-trip/chicago/IMG_2299.jpg",
+        src: "https://lh3.googleusercontent.com/U4YLRTbCJyo1SWXAmudJ0EcGwy5YRi7KgIPW1GsV6BfaYXJ-AOwhVHm78CQyutdAIu4hbGU2Vq4u_C5fCqqcuTOCsPPe7e3P_KDGL4Y3qBBbcB7x0_oVBHhs47WTszaoufIsyqDk=w2400",
         caption: "Untitled"
     },
 ]
