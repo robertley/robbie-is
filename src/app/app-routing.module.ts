8c0e29ecce285408416502a82280bf8f60caee95
robertley <robert.ley94@gmail.com>
2023-07-31 14:44:32 -0400

    removed road trip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,115 +18,115 @@ import { RoadTripComponent } from './components/road-trip/road-trip.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'on-a-road-trip', component: RoadTripComponent,
-    children: [
-      { path: '', pathMatch: 'full', redirectTo: 'utah' },
-      {
-        path: 'tile',
-        component: TileComponent
-      },
-      {
-        path: 'tile/:page',
-        component: TileComponent
-      },
-      {
-        path: 'cedar-point',
-        component: CedarPointComponent
-      },
-      {
-        path: 'cedar-point/:page',
-        component: CedarPointComponent
-      },
-      {
-        path: 'chicago',
-        component: ChicagoComponent
-      },
-      {
-        path: 'chicago/:page',
-        component: ChicagoComponent
-      },
-      {
-        path: 'minneapolis',
-        component: MinneapolisComponent
-      },
-      {
-        path: 'minneapolis/:page',
-        component: MinneapolisComponent
-      },
-      {
-        path: 'badlands',
-        component: BadlandsComponent
-      },
-      {
-        path: 'badlands/:page',
-        component: BadlandsComponent
-      },
-      {
-        path: 'idaho',
-        component: IdahoComponent
-      },
-      {
-        path: 'idaho/:page',
-        component: IdahoComponent
-      },
-      {
-        path: 'oregon',
-        component: OregonComponent
-      },
-      {
-        path: 'oregon/:page',
-        component: OregonComponent
-      },
-      {
-        path: 'redwoods',
-        component: RedwoodsComponent
-      },
-      {
-        path: 'redwoods/:page',
-        component: RedwoodsComponent
-      },
-      {
-        path: 'bay-area',
-        component: BayAreaComponent
-      },
-      {
-        path: 'bay-area/:page',
-        component: BayAreaComponent
-      },
-      {
-        path: '29-palms',
-        component: TwentyninePalmsComponent
-      },
-      {
-        path: '29-palms/:page',
-        component: TwentyninePalmsComponent
-      },
-      {
-        path: 'puerto-vallarta',
-        component: PuertoVallartaComponent
-      },
-      {
-        path: 'puerto-vallarta/:page',
-        component: PuertoVallartaComponent
-      },
-      {
-        path: 'arizona',
-        component: ArizonaComponent
-      },
-      {
-        path: 'arizona/:page',
-        component: ArizonaComponent
-      },
-      {
-        path: 'utah',
-        component: UtahComponent
-      },
-      {
-        path: 'utah/:page',
-        component: UtahComponent
-      }
-    ]
-  }
+  // { path: 'on-a-road-trip', component: RoadTripComponent,
+  //   children: [
+  //     { path: '', pathMatch: 'full', redirectTo: 'utah' },
+  //     {
+  //       path: 'tile',
+  //       component: TileComponent
+  //     },
+  //     {
+  //       path: 'tile/:page',
+  //       component: TileComponent
+  //     },
+  //     {
+  //       path: 'cedar-point',
+  //       component: CedarPointComponent
+  //     },
+  //     {
+  //       path: 'cedar-point/:page',
+  //       component: CedarPointComponent
+  //     },
+  //     {
+  //       path: 'chicago',
+  //       component: ChicagoComponent
+  //     },
+  //     {
+  //       path: 'chicago/:page',
+  //       component: ChicagoComponent
+  //     },
+  //     {
+  //       path: 'minneapolis',
+  //       component: MinneapolisComponent
+  //     },
+  //     {
+  //       path: 'minneapolis/:page',
+  //       component: MinneapolisComponent
+  //     },
+  //     {
+  //       path: 'badlands',
+  //       component: BadlandsComponent
+  //     },
+  //     {
+  //       path: 'badlands/:page',
+  //       component: BadlandsComponent
+  //     },
+  //     {
+  //       path: 'idaho',
+  //       component: IdahoComponent
+  //     },
+  //     {
+  //       path: 'idaho/:page',
+  //       component: IdahoComponent
+  //     },
+  //     {
+  //       path: 'oregon',
+  //       component: OregonComponent
+  //     },
+  //     {
+  //       path: 'oregon/:page',
+  //       component: OregonComponent
+  //     },
+  //     {
+  //       path: 'redwoods',
+  //       component: RedwoodsComponent
+  //     },
+  //     {
+  //       path: 'redwoods/:page',
+  //       component: RedwoodsComponent
+  //     },
+  //     {
+  //       path: 'bay-area',
+  //       component: BayAreaComponent
+  //     },
+  //     {
+  //       path: 'bay-area/:page',
+  //       component: BayAreaComponent
+  //     },
+  //     {
+  //       path: '29-palms',
+  //       component: TwentyninePalmsComponent
+  //     },
+  //     {
+  //       path: '29-palms/:page',
+  //       component: TwentyninePalmsComponent
+  //     },
+  //     {
+  //       path: 'puerto-vallarta',
+  //       component: PuertoVallartaComponent
+  //     },
+  //     {
+  //       path: 'puerto-vallarta/:page',
+  //       component: PuertoVallartaComponent
+  //     },
+  //     {
+  //       path: 'arizona',
+  //       component: ArizonaComponent
+  //     },
+  //     {
+  //       path: 'arizona/:page',
+  //       component: ArizonaComponent
+  //     },
+  //     {
+  //       path: 'utah',
+  //       component: UtahComponent
+  //     },
+  //     {
+  //       path: 'utah/:page',
+  //       component: UtahComponent
+  //     }
+  //   ]
+  // }
 ];
 
 @NgModule({
